bff8823244eb8077c2e692593c6171693b1f5341
Ecaterina Revneac <katia.revneac@gmail.com>
2026-09-09 10:02:49 +0300

    Add POST /users signup endpoint

--- finance-api/src/app.module.ts
+++ finance-api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { TransactionsModule } from './transactions/transactions.module.js';
+import { UsersModule } from './users/users.module.js';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [TransactionsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
